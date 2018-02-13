e0a0426043c47d840c5c8a13c5297b425e6b2b6f
oste19 <36084403+oste19@users.noreply.github.com>
2018-02-13 23:28:00 +0100

    Update CADV_test.sol
    Add onlyPayloadSize moodifier. Fix for the ERC20 short address attack.

--- CADV_test.sol
+++ CADV_test.sol
@@ -58,11 +58,21 @@ contract BasicToken is ERC20Basic {
     mapping(address => uint256) balances;
     
     /**
+    * @dev Fix for the ERC20 short address attack.
+    */
+    modifier onlyPayloadSize(uint size) {
+        if(msg.data.length < size + 4) {
+            throw;
+        }
+        _;
+    }
+    
+    /**
      * @dev transfer token for a specified address
      * @param _to The address to transfer to.
      * @param _value The amount to be transferred.
      */
-    function transfer(address _to, uint256 _value) public returns (bool) {
+    function transfer(address _to, uint256 _value) public onlyPayloadSize(32*2) returns (bool) {
         require(_to != address(0));
         require(_value <= balances[msg.sender]);
         
